be3547dd4c8df23301b6ebe33d52c65ddcbda73b
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-02-03 17:40:23 -0500

    groceryList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FoodItem from './components/FoodItem'
 import AddFood from './components/AddFood'
 
@@ -20,20 +20,40 @@ function App() {
     }
   ]);
 
+  useEffect(() => {
+    // if (localStorage.getItem('food') && localStorage.getItem('food') != []){
+    //   const newfood = JSON.parse(localStorage.getItem('food'))
+    //   setFood(newfood)
+    // } else {
+    //   localStorage.setItem('food', JSON.stringify(food) )
+    // }
+  });
+
+  const clearList = () => {
+    const newList = []
+    setFood(newList)
+    localStorage.setItem('food', JSON.stringify(newList) )
+  }
+
   const addItem = item => {
     const newFood = [...food, item ]
     setFood(newFood)
+    localStorage.setItem('food', JSON.stringify(newFood) )
+
   }
 
   const gotItem = index => {
     const newFoods = [...food]
     newFoods[index].got = true;
     setFood(newFoods)
+    localStorage.setItem('food', JSON.stringify(newFoods) )
   }
 
+  if (food === []) return <div className="App"><h2>Grocery List</h2><br/><div className='foodList'><AddFood addItem={addItem} /></div></div>
   return (
     <div className="App">
-      <br/>
+      <h2>Grocery List</h2>
+      <label className='food' onClick={clearList}>X</label>
       <div className='foodList'>
         {food.map((thing, index) => <FoodItem gotItem={gotItem} key={index} food={thing} index={index} /> )}
         <AddFood addItem={addItem} />
